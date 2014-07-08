e40a1ba35a5215536de115bcd831912bb065dfe6
Nick Baugh <niftylettuce@gmail.com>
2014-07-08 02:16:58 -0400

    Upgraded to latest version of Slate, closes #28

--- source/javascripts/app/lang.js
+++ source/javascripts/app/lang.js
@@ -20,12 +20,31 @@ under the License.
   global.activateLanguage = activateLanguage;
 
   function activateLanguage(language) {
-    $("#lang-selector a").removeClass('active');
-    $("#lang-selector a[data-language-name='" + language + "']").addClass('active');
+    if (!language) return;
+    if (language === "") return;
+
+    $(".lang-selector a").removeClass('active');
+    $(".lang-selector a[data-language-name='" + language + "']").addClass('active');
     for (var i=0; i < languages.length; i++) {
       $(".highlight." + languages[i]).hide();
     }
     $(".highlight." + language).show();
+
+    // scroll to the new location of the position
+    $(window.location.hash).get(0).scrollIntoView(true);
+  }
+
+  // if a button is clicked, add the state to the history
+  function pushURL(language) {
+    if (!history) { return; }
+    var hash = window.location.hash;
+    if (hash) {
+      hash = hash.replace(/^#+/, '');
+    }
+    history.pushState({}, '', '?' + language + '#' + hash);
+
+    // save language as next default
+    localStorage.setItem("language", language);
   }
 
   function setupLanguages(l) {
@@ -33,12 +52,11 @@ under the License.
     var defaultLanguage = localStorage.getItem("language");
 
     languages = l;
-    
-    if ((location.search.substr(1) != "") && (jQuery.inArray(location.search.substr(1), languages)) != -1) {
+
+    if ((location.search.substr(1) !== "") && (jQuery.inArray(location.search.substr(1), languages)) != -1) {
       // the language is in the URL, so use that language!
       activateLanguage(location.search.substr(1));
 
-      // set this language as the default for next time, if the URL has no language
       localStorage.setItem("language", location.search.substr(1));
     } else if ((defaultLanguage !== null) && (jQuery.inArray(defaultLanguage, languages) != -1)) {
       // the language was the last selected one saved in localstorage, so use that language!
@@ -47,25 +65,14 @@ under the License.
       // no language selected, so use the default
       activateLanguage(languages[0]);
     }
-
-    // if we click on a language tab, reload the page with that language in the URL
-    $("#lang-selector a").bind("click", function() {
-      window.location.replace("?" + $(this).data("language-name") + window.location.hash);
-      return false;
-    });
-
   }
 
   // if we click on a language tab, activate that language
   $(function() {
-    $("#lang-selector a").on("click", function() {
-      var lang = $(this).data("language-name");
-      var hash = window.location.hash;
-      if (hash) hash = hash.replace(/^#+/, '');
-      // do not reload the page every time the language is changed
-      if (history) history.pushState({}, '', '?' + lang + '#' + hash);
-
-      activateLanguage(lang);
+    $(".lang-selector a").on("click", function() {
+      var language = $(this).data("language-name");
+      pushURL(language);
+      activateLanguage(language);
       return false;
     });
     window.onpopstate = function(event) {
